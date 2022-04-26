dde3194bddc8a76657e13190b9ec76758d02fd05
NadyaKorvin <nadya.korvin@gmail.com>
2022-04-26 12:10:58 +0300

    add data and finder

--- js/emoji.js
+++ js/emoji.js
@@ -1,5 +1,7 @@
+import { data } from "./data.js";
+
 let getEmoji = (icon, title, text) => {
-  let emojiBlockLine = document.getElementsByClassName("emoji__block_line")[0];
+  let emojiBlockLine = document.querySelector(".emoji__block_line");
   let emojiBlock = document.createElement("div");
   emojiBlock.className = "emoji__block";
   emojiBlockLine.append(emojiBlock);
@@ -8,9 +10,27 @@ let getEmoji = (icon, title, text) => {
   emojiBlock.insertAdjacentHTML("afterbegin", "<div>" + icon + "</div>");
 };
 
-getEmoji("&#x1F61C;", "sgrsgr", "aefeg");
-getEmoji("&#x1F61C;", "sthstrh", "srts");
-getEmoji("&#x1F61C;", "siiiii", "yuuiis");
-getEmoji("&#x1F61C;", "siiiii", "yuuiis");
-getEmoji("&#x1F61C;", "siiiii", "yuuiis");
-getEmoji("&#x1F61C;", "siiiii", "yuuiis");
+let getFromData = () => {
+  data.forEach((element) => {
+    getEmoji(element.symbol, element.title, element.keywords);
+  });
+};
+getFromData();
+
+let cardsAll = document.querySelectorAll(".emoji__block");
+let searchInput = document.querySelector(".search__input");
+
+let search = (elem) => {
+  if (elem.target.value.length > 2) {
+    cardsAll.forEach((elemCard) => {
+      if (elemCard.innerText.includes(elem.target.value)) {
+        elemCard.style.display = "flex";
+      } else {
+        elemCard.style.display = "none";
+      }
+    });
+  }
+};
+
+searchInput.addEventListener("input", search);
+
